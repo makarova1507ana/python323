7f45bfd1eaa23820591273ad4ab25268b7bc542c
makarova1507ana <103330304+makarova1507ana@users.noreply.github.com>
2023-07-09 13:13:41 +0400

    Add files via upload

--- ООП/пример с Pygame/Player.py
+++ ООП/пример с Pygame/Player.py
@@ -1,11 +1,32 @@
 import pygame
-BLACK = (0, 0, 0)# цвет в формате rgb этот красный
+BLACK = (0, 0, 0)# цвет в формате rgb этот черный
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, width=50, height=50):
         pygame.sprite.Sprite.__init__(self) # создание спрайта - игровой объект
-        self.image = pygame.Surface((50, 50)) # размеры игрока
+        self.image = pygame.Surface((width, height)) # размеры игрока
         self.image.fill(BLACK) # цвет
         self.rect = self.image.get_rect()#  получаем его положение
-       # self.image.get_width = 50 # ширина игрока
         self.rect.center = (100,100) # передаем координаты центра игрока расположения
+    
+    #влияет на отрисовку игрока
+    def update(self):#
+        self.run() #отрисовываем перемещение игрока
+    
+    #передвижение игрока
+    def run(self):
+        keystate = pygame.key.get_pressed()#узнаем какую клавишу пользователь
+        # print(keystate)
+        if keystate[pygame.K_DOWN]:# стрелка вниз
+            self.rect.y += 3 #изменяем текущее положение
+        if keystate[pygame.K_LEFT]:# стрелка влево
+            self.rect.x -= 3 #изменяем текущее положение
+        if keystate[pygame.K_RIGHT]:
+            self.rect.x += 3 #изменяем текущее положение
+        if keystate[pygame.K_UP]:
+            self.rect.y -= 3 #изменяем текущее положение
+    
+    #положение игрока
+    def check_walls(self):
+        self.rect.top 
+        self.rect.bottom
--- ООП/пример с Pygame/main.py
+++ ООП/пример с Pygame/main.py
@@ -1,5 +1,7 @@
 import pygame
 from Player import Player
+from Enemy import * #from модуль import конкретный класс или фукнция
+
 pygame.init()# точка входа и настройка игры
 
 
@@ -11,23 +13,36 @@ COLOR = (255, 0, 0)# цвет в формате rgb этот красный
 
 
 #создать игрока
-player = Player()
+player = Player(100,100)
+
+#создать врага
+enemy = Enemy(100,100)
+
 all_sprites = pygame.sprite.Group() # создаем группу спрайтов
+sprites_enemy = pygame.sprite.Group() # создаем группу спрайтов врага
+
 window = pygame.display.set_mode((WIDTH, HEIGHT))# задаем размеры окну
 clock = pygame.time.Clock()# задаем время игры сейчас оно нужно для фпс
 run = True # управление игрой
 
-all_sprites.add(player)
-
+all_sprites.add(player, enemy)
+sprites_enemy.add(enemy)
 #сама игра
 while run:
     for i in pygame.event.get():# отслеживаем события, которые просходят в окне нажатие, закрытие и т.дю
         if i.type == pygame.QUIT:# если окно закрыли 
             run = False# выходим из цикла
+            
+    hit = pygame.sprite.spritecollide(player,sprites_enemy,False)# было столкновение
     
-    
+    #if enemy.rect.left <= player.rect.right and (player.rect.bottom>=enemy.rect.top>= player.rect.top) and( player.rect.bottom>=enemy.rect.bottom>= player.rect.top):
+    if hit:    
+        player.kill()# уничтожили игрока
+        #break
     window.fill(COLOR)#красим окно здесь же можно потом картинки загружать
     all_sprites.draw(window)
+    player.update()#обновляем положение игрока
+    enemy.update()#обновляем положение врага
     pygame.display.update()#обновляем экран каждую итерацию
     clock.tick(FPS)#плавно обновляем
 
