920a0d312906a2c52eff2e4e706a09aba1316d30
makarova1507ana <103330304+makarova1507ana@users.noreply.github.com>
2023-09-10 12:43:29 +0400

    Add files via upload

--- SQL/кнб/KNB_game.py
+++ SQL/кнб/KNB_game.py
@@ -21,21 +21,22 @@ games = [{
   "player_score": 0,
   "computer_score": 1
   "step": 1 
-  "id_player ": 1
+  "id_player": 1
 }]
 """
 for row in range(len(games)):
-    games[row] = {"id": games[row][0], "player_score": games[row][1],"computer_score": games[row][2], "step": games[row][3], "id_player ": games[row][4]}
+    games[row] = {"id": games[row][0], "player_score": games[row][1],"computer_score": games[row][2], "step": games[row][3], "id_player": games[row][4]}
 print(games)
 with sqlite3.connect("knb.db") as con:
     cur = con.cursor()
-    cur.execute("""
+    cur.execute("DROP TABLE IF EXISTS GAMES")
+    cur.executescript("""
                 CREATE TABLE IF NOT EXISTS players(
                   id INTEGER PRIMARY KEY AUTOINCREMENT,
                   pname VARCHAR(50) NOT NULL  );
                   
                 CREATE TABLE IF NOT EXISTS games(
-                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  id INTEGER,
                   player_score INTEGER,
                   computer_score INTEGER,
                   step INTEGER,
@@ -65,9 +66,11 @@ with sqlite3.connect("knb.db") as con:
     # 4 СПОСОБ
     # cur.execute("INSERT INTO players VALUES (NULL, :player_name)", {'player_name': "Egor"})
 
-
+    # for game in games:
+    #   cur.execute("INSERT INTO games VALUES (:id, :player_score, :computer_score, :step, :id_player)", {"id": game["id"], "player_score":game["player_score"], "computer_score":game["computer_score"], "step":game["step"], "id_player":game["id_player"]})
+    
     result = cur.execute("""
-                SELECT * FROM players;
+                SELECT * FROM games;
                 """) 
     for row in result.fetchall():
         print(row)
