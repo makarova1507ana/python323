38759930a48c64f4f4590552772ed57a1bb4248b
makarova1507ana <103330304+makarova1507ana@users.noreply.github.com>
2023-09-16 14:19:07 +0400

    Add files via upload

---
+++ SQL/practice to-do-list user/main.py
@@ -0,0 +1,110 @@
+import random 
+import sqlite3 as sq
+""" 
+1. создать бд для "списка задач"
+
+2. реализовать регистрацию пользователей
+
+3. сделаем имитицию авторизации
+
+4. реализовать интерфейс по управлению задач
+"""
+def add_task(text, id_user):
+    #  добавить проверка на пустую задача
+    return f"INSERT INTO tasks (task, id_user) VALUES ('{text}', {id_user})"
+
+    
+with sq.connect("to-do_list.db") as con:
+    cur = con.cursor()
+ 
+    cur.executescript("""
+        CREATE TABLE IF NOT EXISTS users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE  NOT NULL,
+            password TEXT NOT NULL,
+            avatar BLOB);
+            
+        CREATE TABLE IF NOT EXISTS tasks(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            task TEXT NOT NULL,
+            id_user INTEGER NOT NULL,
+            FOREIGN KEY (id_user) REFERENCES users(id));
+        """)
+    
+    
+    #---------------------Регистрация пользователя---------------------------#
+    # создали админа -  просто так
+    # cur.execute("""
+    #             INSERT INTO users (username, password) VALUES ("admin", "admin1234")
+    #             """) 
+    
+    name_user = "username257"#"username" + str(random.randint(1,1000)) # пользователь заносит данные например через input
+    result = cur.execute(f"""
+                SELECT username FROM users WHERE username = \'{name_user}\'
+                """) 
+    # print(result.fetchall())
+    
+    
+    while len(result.fetchall()) != 0:
+        name_user = "username" + str(random.randint(1,1000)) # пользователь заносит данные например через input
+        result = cur.execute(f"""
+                SELECT username FROM users WHERE username = \'{name_user}\'
+                """)
+    password = name_user
+    
+    # преобразуем к двоичному формату
+    with open("diagram2.jpg","rb") as f:
+        bin_file = f.read()
+
+    avatar = bin_file
+    
+    cur.execute("INSERT INTO users (username, password, avatar) VALUES (?,?,?)",(name_user, password, avatar)) 
+    
+    
+    
+    #---------------------КОНЕЦ  Регистрация пользователя---------------------------#
+
+
+
+
+    #---------------------имитация Авторизации пользователя---------------------------#
+    username = "username257"
+    result = cur.execute(f"""
+                SELECT id,username FROM users WHERE username = \'{name_user}\'
+                """)
+    while len(result.fetchall()) != 0:
+      #  "4. реализовать интерфейс по управлению задач"
+      comand = input("""
+                     1 - add task
+                     2 - update task 
+                     3 - delete task
+                     4 - exit 
+                     """) 
+      if comand ==  "1":
+        pass
+          # !!! НЕ РАБОТАЕТ !!! 
+        #   result = cur.execute(f"""
+        #         SELECT id,username FROM users WHERE username = \'{name_user}\'
+        #         """)
+        #   #task = add_task("task1",result.fetchall()[0][0])
+        #   result = cur.execute(f"""
+        #         SELECT id,username FROM users WHERE username = \'{name_user}\'
+        #         """)
+    else:
+        print("не удалось авторизоваться",)
+        
+        
+    result = cur.execute("""
+                SELECT * FROM users;
+                """) 
+    # db -> SQL 
+    with open("result.sql","w") as f:
+        for sql_txt in con.iterdump():
+            f.write(sql_txt)
+
+# #прочитать sql 
+#     with open("result.sql","r") as f:
+#         sql_script = f.read() 
+#         cur.executescript(sql_script)           
+    # for row in result.fetchall():
+    #     print(row)
