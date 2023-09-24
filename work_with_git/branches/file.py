0e0e6ba582b5e92ec00d037df4e3b20bc1a4a900
makarova1507ana <103330304+makarova1507ana@users.noreply.github.com>
2023-09-24 14:08:45 +0400

    Add files via upload

---
+++ work_with_git/branches/file.py
@@ -0,0 +1,8 @@
+# work with branches
+
+# создала новую строку 
+
+#находимся во второй ветке
+
+
+#Создали две ветки и 5 коммитов  схема  изображена https://app.conceptboard.com/board/2gs2-megz-mdhn-xks3-33nc
---
+++ work_with_git/lesson1.py
@@ -0,0 +1,14 @@
+#Создайте папку "my first local repositiry" на рабочем столе
+#Создайте в ней текстовый файл "repository.txt", запишите в него текст "git comands"
+
+
+# статусы отслеживания
+
+#untrackted 
+#unmodified
+#modified
+#stared 
+
+
+
+#перемещение между версиями
