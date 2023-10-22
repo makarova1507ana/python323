39e6dc59ca76c774ad8f5e8a0984a36764f8ad1e
makarova1507ana <103330304+makarova1507ana@users.noreply.github.com>
2023-10-22 17:45:16 +0400

    Add files via upload

--- newkinopoisk/films/urls.py
+++ newkinopoisk/films/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path
 import films.views as film  #from films.views import index, movie, categories
 
 urlpatterns = [
     path('', film.index),  # http://127.0.0.1:8000/films/
     path('movie/<int:id_movie>/', film.movie), # http://127.0.0.1:8000/films/movie/{id}/
     path('categories/<slug:cat_name>/', film.categories), # http://127.0.0.1:8000/films/categories/{cat_name}/
+
+    # http://127.0.0.1:8000/films/archive/2000 - валидный
+    re_path(r"^archive/(?P<year>(1|2)\d{3})/$", film.archive),
+    #  1000 - 2999 - валидный
+    # http://127.0.0.1:8000/films/archive/434324234 - невалидный
 
 ]
--- newkinopoisk/films/views.py
+++ newkinopoisk/films/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,9 +6,19 @@ def index(request):# http://127.0.0.1:8000/films/
     return HttpResponse("Страница про фильмы")
 
 def movie(request, id_movie):# http://127.0.0.1:8000/films/movie/{id_movie}
-    return HttpResponse(f"Страница про {id_movie} фильм")
+    data = {"header1": "страница про фильм",
+            "id": id_movie
+            }
+     #данные из модели (БД)
+    return render(request, "films/index.html", data)
 
 
 def categories(request, cat_name):# http://127.0.0.1:8000/films/movie/{id_movie}
     return HttpResponse(f"Страница про {cat_name} категория ")
+
+def archive(request, year):
+    return HttpResponse(f"archive {year}")
+
+    # if int(year) < 2023: return HttpResponse(f"archive {year}")
+    # return Http404
 
